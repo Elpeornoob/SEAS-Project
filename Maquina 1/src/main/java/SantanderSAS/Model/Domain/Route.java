a15b29ea6ca1a360acb1a89e9aa0693ff2786ae8
KamiTol <gkieffer27@gmail.com>
2024-04-20 15:50:41 -0500

    Implementación de Grafo y clase Route

--- Maquina 1/src/main/java/SantanderSAS/Model/Domain/Route.java
+++ Maquina 1/src/main/java/SantanderSAS/Model/Domain/Route.java
@@ -1,4 +1,37 @@
 package SantanderSAS.Model.Domain;
 
 public class Route {
+    private String start;
+    private String end;
+    private int distance;
+
+    public Route(String start, String end, int distance) {
+        this.start = start;
+        this.end = end;
+        this.distance = distance;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 }
