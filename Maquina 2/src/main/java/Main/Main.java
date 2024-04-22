abe0e01ed6627b39e8991fff796b12b0fe53c358
Gian Kieffer Toledo De La Crúz <gkieffer27@gmail.com>
2024-04-21 22:46:48 -0500

    Adelanto Sistema de Tiquetes.

---
+++ Maquina 2/src/main/java/Main/Main.java
@@ -0,0 +1,38 @@
+package Main;
+
+import Modelo.TicketModel;
+import Vista.TicketView;
+import Controller.TicketController;
+import SantanderSAS.Model.Domain.Route;
+import SantanderSAS.Model.Domain.Passenger.Passenger;
+import SantanderSAS.Model.Domain.Passenger.ContactPerson;
+import SantanderSAS.Model.Domain.Passenger.Baggage.Baggage;
+
+public class Main {
+    public static void main(String[] args) {
+        // create a new ticket model
+        String id = "someId";
+        String dateHourbuy = "someDateHourbuy";
+        String dateHourDeparture = "someDateHourDeparture";
+        String dateHourArrival = "someDateHourArrival";
+
+        // Create instances using constructors
+        Route route = new Route("start", "end", 100);
+        Passenger passenger = new Passenger("name", "lastName", new String[]{"1234567890"}, null, "currentAddress");
+        ContactPerson contactPerson = new ContactPerson("name", "lastName", new String[]{"1234567890"});
+        Baggage baggage = new Baggage("baggageInfo", "baggageId", "weight", "wagonId");
+
+        String ticketCategory = "someTicketCategory";
+        String idTrain = "someIdTrain";
+        String place = "somePlace";
+        String ticketCost = "someTicketCost";
+        int seat = 1;
+
+        TicketModel model = new TicketModel(id, dateHourbuy, dateHourDeparture, dateHourArrival, route, passenger, ticketCategory, idTrain, place, ticketCost, contactPerson, baggage, seat);
+
+        // create a view to get ticket details from user
+        TicketView view = new TicketView();
+        // create a controller
+        TicketController controller = new TicketController(model, view);
+    }
+}
